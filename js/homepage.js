2d4df3c5e6beca4faf07048ee04707a1081bbb1f
Tobi <tobias.ludwig@freudenberg.com>
2023-01-15 18:13:41 +0100

    Varaiblen hinzugefügt

--- js/homepage.js
+++ js/homepage.js
@@ -5,7 +5,7 @@ $(function () {
         "id":"marke",
     //    "labelText":"Standorte",
         "listValues":{"Marke": "", "Mercedes": "Mercedes", "BMW":"BMW", 
-        "100€ - 200€":"100€ - 200€", "200€ - 500€": "200€ - 500€"},
+        "VW":"VW", "Audi": "Audi"},
     });
 });
 
@@ -51,37 +51,81 @@ $(function () {
     });
 });
 
-
-var imageButtonHolder1;
+//Autobild1
 $(function () {
     imageButtonHolder1 = createImageButtonHolder({
                 "parent": $("#auto1"),
                 "singleActive": false,
                 "width": "400px",
                 "padding": "5px",
-                "margin": "10px"
+                "margin": "10px",
+                "onClick": autoClick
             });
-    imageButtonHolder1.addImageButton("Auto1","../img/AMG.jpg");
 
 });
 
-var imageButtonHolder2;
-
+//Autobild2
 $(function () {
     imageButtonHolder2 = createImageButtonHolder({
                 "parent": $("#auto2"),
                 "singleActive": false,
                 "width": "400px",
                 "padding": "5px",
-                "margin": "10px"
+                "margin": "10px",
+                "onClick": autoClick
             });
-    imageButtonHolder2.addImageButton("Auto2","../img/G-Klasse.jpg");
-
 });
+
+//Autobild3
+$(function () {
+    imageButtonHolder3 = createImageButtonHolder({
+                "parent": $("#auto3"),
+                "singleActive": false,
+                "width": "400px",
+                "padding": "5px",
+                "margin": "10px",
+                "onClick": autoClick
+            });
+});
+
+//Autobild4
+$(function () {
+    imageButtonHolder4 = createImageButtonHolder({
+                "parent": $("#auto4"),
+                "singleActive": false,
+                "width": "400px",
+                "padding": "5px",
+                "margin": "10px",
+                "onClick": autoClick
+            });
+});
+
+function autoClick()  //Aufruf der Autoansichtseite beim klicken auf ein Auto
+{
+    window.open("autoansicht.html")
+    // window.close()
+}
 
 
 function SuchButtonFunction()
 {
+    //Daten aus dem Backend
+    let autoname1 = "Auto1"
+    let autoname2 = "Auto2"
+    let autoname3 = "Auto3"
+    let autoname4 = "Auto4"
+    let autourl1 = "../img/AMG.jpg"
+    let autourl2 = "../img/G-Klasse.jpg"
+    let autourl3 = "../img/porsche1.jpg"
+    let autourl4 = "../img/porsche2.jpg"
+
+    //Erstellen der Buttons der Autos
+    imageButtonHolder1.addImageButton(autoname1, autourl1);
+    imageButtonHolder2.addImageButton(autoname2, autourl2);
+    imageButtonHolder3.addImageButton(autoname3, autourl3);
+    imageButtonHolder4.addImageButton(autoname4, autourl4);
+
+
     let marke = Marke.getValue()
     let standort = Standort.getValue()
     let preis = Preis.getValue()
@@ -92,5 +136,7 @@ function SuchButtonFunction()
     document.getElementById('varverfügbarkeit').innerHTML = verfügbarkeit;
 
 }
+
+
 
 
