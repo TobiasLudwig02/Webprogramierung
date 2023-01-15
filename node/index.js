a491e8474a57a06c40c6c900eda857cfe3892aa6
ankiit0 <96061554+ankiit0@users.noreply.github.com>
2023-01-15 20:38:12 +0100

    Added node

---
+++ node/index.js
@@ -0,0 +1,12 @@
+// index.js
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/:yourName', (req, res) => {
+  res.send('Your name: Ankit ' + req.params.yourName);
+});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
