fd93ada8273cf770e945c44727e23d3f745f4ea2
TobiasLudwig02 <tobias.ludwig@freudenberg.com>
2023-01-16 13:16:01 +0100

    Replaced Logout

--- js/checklogin.js
+++ js/checklogin.js
@@ -1,11 +1,11 @@
 function checkLogin()
     {
-        if (localStorage.getItem("storeEmail") !== null && localStorage.getItem("storePassword") !== null)
-        {}
-        else 
-        {
-            window.open("login.html");
-            window.close();
-        }
+        // if (localStorage.getItem("storeEmail") !== null && localStorage.getItem("storePassword") !== null)
+        // {}
+        // else 
+        // {
+        //     window.open("login.html");
+        //     window.close();
+        // }
 
     }
