c1e2fbe585af17a7d1c49b1cfc572d147221c1da
TobiasLudwig02 <tobias.ludwig@freudenberg.com>
2023-01-16 20:43:17 +0100

    Tried Node

--- node/index.js
+++ node/index.js
@@ -1,12 +1,48 @@
-// index.js
+var express = require('express');
+var app = express();
+var fs = require("fs");
+const port = 8081
 
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+var user = {
+  "user3" : {
+    "id": 3,
+    "name" : "Test2",
+    "vorname": "Test3",
+    "strasse" : "Test4",
+    "hausnummer" : "Tes2t",
+    "Wohnort" : "Tes2t",
+    "plz" : "Te2st",
+    "land" : "Te1st",
+    "führerschein" : "Test2",
+    "email" : "Tes23t@web.de",
+    "passwort" : "Test23321123"
+  }
+}
 
-app.get('/:yourName', (req, res) => {
-  res.send('Your name: Ankit ' + req.params.yourName);
-});
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.post('/addUser', function (req, res) {
+//   // First read existing users.
+//   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+//      data = JSON.parse( data );
+//      data["user3"] = user["user3"];
+//     //  console.log( data );
+//      res.end( JSON.stringify(data));
+//   });
+// })
+
+app.get('/listUsers', function (req, res) {
+  fs.readFile( "users.json", 'utf8', function (err, data) {
+     // console.log( data );
+     res.end( data );
+  });
+})
+
+app.get("/listUsers/id", (req, res) =>{
+  res.send(req.params.id)
+} )
 
 
+var server = app.listen(port, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
